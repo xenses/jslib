fb6852c66bef8744ab3c97babe347b9014dddf36
Shawn Boyette <shawn@firepear.net>
2011-07-17 17:07:39 -0400

    make some messages better

--- test/chunker_test.js
+++ test/chunker_test.js
@@ -6,16 +6,16 @@ load("../chunker.js");
 
 // instantiate chunker
 var c = new chunker;
-isnt(c, undefined,  "Object instantiation failed (undef)");
-is(typeof c, 'object', "Object instantiation failed (bad type)");
-is(c instanceof chunker, true, "Object instantiation failed (bad instance)");
+isnt(c, undefined,  "c should exist");
+is(typeof c, 'object', "..and it should be an object");
+is(c instanceof chunker, true, "...and it should be a 'chunker' instance");
 
 // try parsing a null document
 is(c.chunk(''), 0, "Parsing null document should yield zero");
 is (c.chunkCount, 0, "curChunk.chunkCount should be 0 after chunk('')");
 
 // ok, let's go for a minimal-ish contentful doc
-is(c.chunk("#test doc 1\nchunk 1\n#\nchunk 2\n#\nchunk 3"), 3, "chunk() should return 3");
+is(c.chunk("#test doc 1\nchunk 1\n#\nchunk 2\n#\nchunk 3"), 3, "chunk() should return 3 here");
 is(c.chunks instanceof Array, true, "curChunk.chunks should be instanceof Array");
 is(c.chunks.length, 3, "curChunk.chunks.length should be 3");
 is(c.chunks[0]['content'], "chunk 1", "curChunk.chunks[0]['content'] mismatch");
@@ -23,7 +23,7 @@ is(c.chunks[1]['content'], "chunk 2", "curChunk.chunks[1]['content'] mismatch");
 is(c.chunks[2]['content'], "chunk 3", "curChunk.chunks[2]['content'] mismatch");
 
 // make sure blank lines before content are ignored
-is(c.chunk("\n\n\n\n#\n\n\nchunk 1\n#\nchunk\n\nnumber\n2"), 2, "chunk() should return 2");
+is(c.chunk("\n\n\n\n#\n\n\nchunk 1\n#\nchunk\n\nnumber\n2"), 2, "chunk() should return 2 here");
 is(c.chunks instanceof Array, true, "curChunk.chunks should be instanceof Array");
 is(c.chunks.length, 2, "curChunk.chunks.length should be 2");
 is(c.chunks[0]['content'], "chunk 1", "chunks[0]['content'] mismatch");
