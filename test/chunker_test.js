8995224abceba53621181f3bc5d65a261e5a8dcb
Shawn Boyette <shawn@firepear.net>
2011-07-18 10:58:43 -0400

    more tests

--- test/chunker_test.js
+++ test/chunker_test.js
@@ -18,16 +18,33 @@ is (c.chunkCount, 0, "curChunk.chunkCount should be 0 after chunk('')");
 is(c.chunk("#test doc 1\nchunk 1\n#\nchunk 2\n#\nchunk 3"), 3, "chunk() should return 3 here");
 is(c.chunks instanceof Array, true, "curChunk.chunks should be instanceof Array");
 is(c.chunks.length, 3, "curChunk.chunks.length should be 3");
-is(c.chunks[0]['content'], "chunk 1", "curChunk.chunks[0]['content'] mismatch");
-is(c.chunks[1]['content'], "chunk 2", "curChunk.chunks[1]['content'] mismatch");
-is(c.chunks[2]['content'], "chunk 3", "curChunk.chunks[2]['content'] mismatch");
+is(c.chunks[0].content, "chunk 1", "curChunk.chunks[0].content mismatch");
+is(c.chunks[1].content, "chunk 2", "curChunk.chunks[1].content mismatch");
+is(c.chunks[2].content, "chunk 3", "curChunk.chunks[2].content mismatch");
 
 // make sure blank lines before content are ignored
 is(c.chunk("\n\n\n\n#\n\n\nchunk 1\n#\nchunk\n\nnumber\n2"), 2, "chunk() should return 2 here");
 is(c.chunks instanceof Array, true, "curChunk.chunks should be instanceof Array");
 is(c.chunks.length, 2, "curChunk.chunks.length should be 2");
-is(c.chunks[0]['content'], "chunk 1", "chunks[0]['content'] mismatch");
-is(c.chunks[1]['content'], "chunk\n\nnumber\n2", "chunks[1]['content'] mismatch");
+is(c.chunks[0].content, "chunk 1", "chunks[0].content mismatch");
+is(c.chunks[1].content, "chunk\n\nnumber\n2", "chunks[1].content mismatch");
+
+// let's go k/v directives
+is(c.chunk("#foo:bar\none\n#a:1\n#b:2\n#c:3 4 5\ntwo\n#\nthree"), 3, "three chunks, two with metadata");
+is(c.chunks[0].content, "one", "first chunk content");
+is(typeof c.chunks[0].meta, "object", "first chunk should have metadata");
+is(c.chunks[0].meta.foo, "bar", "first chunk should have one metadata item named foo, value bar");
+is(c.chunks[0].meta.bar, undefined, "first chunk shouldn't have any other metadata (1)");
+is(c.chunks[0].meta.a, undefined, "first chunk shouldn't have any other metadata (2)");
+is(c.chunks[1].content, "two", "second chunk content");
+is(typeof c.chunks[1].meta, "object", "second chunk should have metadata (1)");
+is(Object.keys(c.chunks[1].meta), 'a,b,c', "2nd chunk should have metadata (2)");
+is(c.chunks[1].meta.a, 1, "2nd chunk meta.a");
+is(c.chunks[1].meta.b, 2, "2nd chunk meta.b");
+is(c.chunks[1].meta.c, "3 4 5", "2nd chunk meta.c");
+is(c.chunks[2].content, "three", "3rd chunk content");
+is(Object.keys(c.chunks[2].meta), '', "3rd chunk should not have metadata");
+
 
 // end-of-run
 testPrintSummary();
